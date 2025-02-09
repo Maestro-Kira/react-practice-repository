27182d37696b5cd94497d44f2c5c04eafc77dfc0
Maestro-Kira <121399822+Maestro-Kira@users.noreply.github.com>
2025-02-09 13:02:25 +0200

    Practice react coditinal rendering cart example

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
-const ValidPassword = () => <h1>Valid Password</h1>;
+const Cart = () => {
+  const cart = ["Apples", "Oranges", "Tomatoes"];
+  return (
+    <div>
+      <h1>CART 🛒</h1>
 
-const InvalidPassword = () => <h1>inValid Password</h1>;
+      {cart.length > 0 && <h2>You have {cart.length} items in your cart</h2>}
 
-const Password = ({ isValid }) =>
-  isValid ? <ValidPassword /> : <InvalidPassword />;
+      <ul>
+        <h4>Products</h4>
+        {cart.map((item) => (
+          <li key={Math.random()}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div>
-      <Password isValid={false} />
+      <Cart />
     </div>
   );
 }
