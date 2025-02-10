030687dcff9f532628aa97ebd70785960af1267a
Maestro-Kira <121399822+Maestro-Kira@users.noreply.github.com>
2025-02-10 15:24:00 +0200

    Practice react useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
-const Copy = () => {
-  const copyHandler = () => {
-    alert("DON'T COPY!!");
-  };
-
-  return (
-    <p onCopy={copyHandler}>
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti maxime
-      corrupti eos odio, eius non ea nobis itaque facere sequi?
-    </p>
-  );
-};
+import { useState } from "react";
 
 function App() {
+  const [friends, setFriends] = useState(["Alex", "Sam", "John"]);
+  const addOneFriend = () => setFriends([...friends, "Kira"]);
+  const removeOneFriend = () =>
+    setFriends(friends.filter((friend) => friend !== "John"));
+  const updateOneFriend = () =>
+    setFriends(
+      friends.map((friend) => (friend === "Alex" ? "Alex Smith" : friend))
+    );
+
   return (
     <section>
-      <Copy />
+      {friends.map((friend) => (
+        <li key={Math.random()}>{friend}</li>
+      ))}
+
+      <button onClick={addOneFriend}>Add New Friend</button>
+      <button onClick={removeOneFriend}>Remove One Friend</button>
+      <button onClick={updateOneFriend}>Update one friend</button>
     </section>
   );
 }
