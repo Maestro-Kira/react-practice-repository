54994591d235efc3f57c3aa1e52a2a86f7140f7d
Maestro-Kira <121399822+Maestro-Kira@users.noreply.github.com>
2025-02-11 13:04:42 +0200

    Practice react useState 3 examples

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,13 @@
-import { useState } from "react";
+import ExampleOne from "./components/ExampleOne";
+import ExampleThree from "./components/ExampleThree";
+import ExampleTwo from "./components/ExampleTwo";
 
 function App() {
-  const [movies, setMovies] = useState([
-    { id: 1, title: "Spider Man", ratings: 3 },
-    { id: 2, title: "Superman ", ratings: 4 },
-    { id: 3, title: "Ant Man", ratings: 5 },
-  ]);
-
-  const handleClick = () => {
-    setMovies(
-      movies.map((movie) =>
-        movie.id === 1 ? { ...movies, title: "John Wick 4" } : movie
-      )
-    );
-  };
-
   return (
     <section>
-      {movies.map((movie) => (
-        <li key={movie.id}>
-          {movie.title} - Rating {movie.ratings}
-        </li>
-      ))}
-
-      <button onClick={handleClick}>Change name</button>
+      <ExampleOne />
+      <ExampleTwo />
+      <ExampleThree />
     </section>
   );
 }
