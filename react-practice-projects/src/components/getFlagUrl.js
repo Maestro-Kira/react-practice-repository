a4dc6ba65fbb361c2c3a8f6b74a11da85b63019b
Maestro-Kira <121399822+Maestro-Kira@users.noreply.github.com>
2025-02-27 00:43:11 +0200

    Refactor code separate getFlagUrl to separate file.

---
+++ react-practice-projects/src/components/getFlagUrl.js
@@ -0,0 +1,67 @@
+const getFlagUrl = (area) => {
+    switch (area) {
+      case "American":
+        return "https://flagsapi.com/US/flat/64.png";
+      case "British":
+        return "https://flagsapi.com/GB/flat/64.png";
+      case "Canadian":
+        return "https://flagsapi.com/CA/flat/64.png";
+      case "Chinese":
+        return "https://flagsapi.com/CN/flat/64.png";
+      case "Croatian":
+        return "https://flagsapi.com/HR/flat/64.png";
+      case "Dutch":
+        return "https://flagsapi.com/NL/flat/64.png";
+      case "Egyptian":
+        return "https://flagsapi.com/EG/flat/64.png";
+      case "Filipino":
+        return "https://flagsapi.com/PH/flat/64.png";
+      case "French":
+        return "https://flagsapi.com/FR/flat/64.png";
+      case "Greek":
+        return "https://flagsapi.com/GR/flat/64.png";
+      case "Indian":
+        return "https://flagsapi.com/IN/flat/64.png";
+      case "Irish":
+        return "https://flagsapi.com/IE/flat/64.png";
+      case "Italian":
+        return "https://flagsapi.com/IT/flat/64.png";
+      case "Jamaican":
+        return "https://flagsapi.com/JM/flat/64.png";
+      case "Japanese":
+        return "https://flagsapi.com/JP/flat/64.png";
+      case "Kenyan":
+        return "https://flagsapi.com/KE/flat/64.png";
+      case "Malaysian":
+        return "https://flagsapi.com/MY/flat/64.png";
+      case "Mexican":
+        return "https://flagsapi.com/MX/flat/64.png";
+      case "Moroccan":
+        return "https://flagsapi.com/MA/flat/64.png";
+      case "Norwegian":
+        return "https://flagsapi.com/NO/flat/64.png";
+      case "Polish":
+        return "https://flagsapi.com/PL/flat/64.png";
+      case "Portuguese":
+        return "https://flagsapi.com/PT/flat/64.png";
+      case "Russian":
+        return "https://flagsapi.com/RU/flat/64.png";
+      case "Spanish":
+        return "https://flagsapi.com/ES/flat/64.png";
+      case "Thai":
+        return "https://flagsapi.com/TH/flat/64.png";
+      case "Tunisian":
+        return "https://flagsapi.com/TN/flat/64.png";
+      case "Turkish":
+        return "https://flagsapi.com/TR/flat/64.png";
+      case "Ukrainian":
+        return "https://flagsapi.com/UA/flat/64.png";
+      case "Uruguayan":
+        return "https://flagsapi.com/UY/flat/64.png";
+      case "Vietnamese":
+        return "https://flagsapi.com/VN/flat/64.png";
+      default:
+        return "https://flagsapi.com/MD/flat/64.png";
+    }
+  };
+export default getFlagUrl
