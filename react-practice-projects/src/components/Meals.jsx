481729a63750bcf3650966679ecb9c42edc8291e
Maestro-Kira <121399822+Maestro-Kira@users.noreply.github.com>
2025-02-27 03:54:44 +0200

    Finish react meal app. Implement wait for loading annotation on both pages. Remove up button when less than 6 items on screen

--- react-practice-projects/src/components/Meals.jsx
+++ react-practice-projects/src/components/Meals.jsx
@@ -1,45 +1,69 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
 import "../style.css";
 import getFlagUrl from "./getFlagUrl";
+import Meals2 from "./Meals2";
+import { useEffect, useState } from "react";
 
 const Meals = () => {
   const [mealCategory, setMealCategory] = useState([]);
-  const [displayCountries, setDislayCountries] = useState(true);
-
+  const [displayCountries, setDisplayCountries] = useState(true);
+  const [displayMeals, setDisplayMeals] = useState(false);
+  const [selectedCountry, setSelectedCountry] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
   useEffect(() => {
     // Fetch meals category country list
     const fetchMealsCategory = async () => {
       try {
+        setIsLoading(true);
         let res = await axios.get(
           "https://www.themealdb.com/api/json/v1/1/list.php?a=list"
         );
         setMealCategory(res.data.meals);
       } catch (error) {
         console.log(`Error during fetch: ${error}`);
+      } finally {
+        setIsLoading(false);
       }
     };
     fetchMealsCategory();
   }, []);
 
-  // Function to get flag URL based on area (country)
-  const hideCard = () => {
-    setDislayCountries(false);
+  // Function to hide main page and display specific country meal
+  const hideCards = (country) => {
+    setSelectedCountry(country);
+    setDisplayCountries(false);
+    setDisplayMeals(true);
   };
   return (
     <div>
       {displayCountries && (
         <section>
           <h1>Please choose meal category:</h1>
-          <ul className="list">
-            {mealCategory.map(({ strArea }) => (
-              <div key={strArea} className="meal-item" onClick={hideCard}>
-                <img src={getFlagUrl(strArea)} alt={`Flag of ${strArea}`} />
-                <li>{strArea}</li>
-              </div>
-            ))}
-          </ul>
+          {isLoading ? (
+            <h2>Loading countries..</h2>
+          ) : (
+            <ul className="list">
+              {mealCategory.map(({ strArea }) => (
+                <div
+                  key={strArea}
+                  className="meal-item"
+                  onClick={() => hideCards(strArea)}
+                >
+                  <img src={getFlagUrl(strArea)} alt={`Flag of ${strArea}`} />
+                  <li>{strArea}</li>
+                </div>
+              ))}
+            </ul>
+          )}
         </section>
+      )}
+
+      {displayMeals && (
+        <Meals2
+          country={selectedCountry}
+          setDisplayMeals={setDisplayMeals}
+          setDisplayCountries={setDisplayCountries}
+        />
       )}
     </div>
   );
