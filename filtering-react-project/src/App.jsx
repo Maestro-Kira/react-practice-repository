a2bfb835d43052d6e871252bdd599b06f256aa4c
Maestro-Kira <121399822+Maestro-Kira@users.noreply.github.com>
2025-03-06 15:42:19 +0200

    Finish recommended styling section styling

--- filtering-react-project/src/App.jsx
+++ filtering-react-project/src/App.jsx
@@ -1,10 +1,12 @@
 // import { v4 as uuidv4 } from "uuid";
 import Navigation from "./Navigation/Navigation";
+import Recommended from "./Recommended/Recommended";
 
 function App() {
   return (
     <div>
       <Navigation />
+      <Recommended />
     </div>
   );
 }
--- filtering-react-project/src/Recommended/Recommended.jsx
+++ filtering-react-project/src/Recommended/Recommended.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import "./Recommended.css";
 
 const Recommended = () => {
-  return <div>Recommended</div>;
+  return (
+    <section className="recommended-section">
+      <h2 className="recommended-title">Recommended</h2>
+      <button className="recommended-btn">All Products</button>
+      <button className="recommended-btn">Nike</button>
+      <button className="recommended-btn">Adidas</button>
+      <button className="recommended-btn">Puma</button>
+      <button className="recommended-btn">Vans</button>
+    </section>
+  );
 };
 
 export default Recommended;
