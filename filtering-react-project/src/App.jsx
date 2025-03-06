2e84fbac3ef8ee09a189309aaf3b536c85911873
Maestro-Kira <121399822+Maestro-Kira@users.noreply.github.com>
2025-03-06 15:14:44 +0200

    Finish navigation section styling

--- filtering-react-project/src/App.jsx
+++ filtering-react-project/src/App.jsx
@@ -1,9 +1,10 @@
 // import { v4 as uuidv4 } from "uuid";
+import Navigation from "./Navigation/Navigation";
 
 function App() {
   return (
     <div>
-      <h1>HELLO</h1>
+      <Navigation />
     </div>
   );
 }
--- filtering-react-project/src/Navigation/Navigation.jsx
+++ filtering-react-project/src/Navigation/Navigation.jsx
@@ -1,7 +1,27 @@
 import React from "react";
-
+import { FaHeart } from "react-icons/fa";
+import { IoCartOutline } from "react-icons/io5";
+import { IoMdPersonAdd } from "react-icons/io";
+import "./Navigation.css";
 const Navigation = () => {
-  return <div>Navigation</div>;
+  return (
+    <>
+      <nav className="nav-section">
+        <input
+          type="text"
+          name="searchBar"
+          id="searchBar"
+          placeholder="Enter your search shoes.."
+          className="nav-search-bar"
+        />
+        <div className="nav-icons-section">
+          <FaHeart className="nav-icons" />
+          <IoCartOutline className="nav-icons" />
+          <IoMdPersonAdd className="nav-icons" />
+        </div>
+      </nav>
+    </>
+  );
 };
 
 export default Navigation;
