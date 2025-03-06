0e4c31e2ee544e51d8fe3a1d1fbd79cae9812eff
Maestro-Kira <121399822+Maestro-Kira@users.noreply.github.com>
2025-03-06 17:10:34 +0200

    Finish products styling section styling

--- filtering-react-project/src/App.jsx
+++ filtering-react-project/src/App.jsx
@@ -1,12 +1,13 @@
 // import { v4 as uuidv4 } from "uuid";
 import Navigation from "./Navigation/Navigation";
 import Recommended from "./Recommended/Recommended";
-
+import Products from "./Products/Products";
 function App() {
   return (
     <div>
       <Navigation />
       <Recommended />
+      <Products />
     </div>
   );
 }
--- filtering-react-project/src/Products/Products.jsx
+++ filtering-react-project/src/Products/Products.jsx
@@ -1,7 +1,188 @@
 import React from "react";
+import "./Products.css";
+import { FaStar } from "react-icons/fa";
+import { FaBasketShopping } from "react-icons/fa6";
 
 const Products = () => {
-  return <div>Products</div>;
+  return (
+    <section className="product-container">
+      <div className="card">
+        <img
+          src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg"
+          alt=""
+          className="product-img"
+        />
+        <span className="product-title">Nike Air Monarch IV</span>
+        <div className="product-reviews-section">
+          <FaStar className="product-star" />
+          <FaStar className="product-star" />
+          <FaStar className="product-star" />
+          <FaStar className="product-star" />
+          <span>(123 reviews)</span>
+        </div>
+        <div className="product-price">
+          <span>
+            <del>$140.00</del> 200
+          </span>
+          <FaBasketShopping className="product-basket" />
+        </div>
+      </div>
+
+      <div className="card">
+        <img
+          src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg"
+          alt=""
+          className="product-img"
+        />
+        <span className="product-title">Nike Air Monarch IV</span>
+        <div className="product-reviews-section">
+          <FaStar className="product-star" />
+          <FaStar className="product-star" />
+          <FaStar className="product-star" />
+          <FaStar className="product-star" />
+          <span>(123 reviews)</span>
+        </div>
+        <div className="product-price">
+          <span>
+            <del>$140.00</del> 200
+          </span>
+          <FaBasketShopping className="product-basket" />
+        </div>
+      </div>
+
+      <div className="card">
+        <img
+          src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg"
+          alt=""
+          className="product-img"
+        />
+        <span className="product-title">Nike Air Monarch IV</span>
+        <div className="product-reviews-section">
+          <FaStar className="product-star" />
+          <FaStar className="product-star" />
+          <FaStar className="product-star" />
+          <FaStar className="product-star" />
+          <span>(123 reviews)</span>
+        </div>
+        <div className="product-price">
+          <span>
+            <del>$140.00</del> 200
+          </span>
+          <FaBasketShopping className="product-basket" />
+        </div>
+      </div>
+
+      <div className="card">
+        <img
+          src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg"
+          alt=""
+          className="product-img"
+        />
+        <span className="product-title">Nike Air Monarch IV</span>
+        <div className="product-reviews-section">
+          <FaStar className="product-star" />
+          <FaStar className="product-star" />
+          <FaStar className="product-star" />
+          <FaStar className="product-star" />
+          <span>(123 reviews)</span>
+        </div>
+        <div className="product-price">
+          <span>
+            <del>$140.00</del> 200
+          </span>
+          <FaBasketShopping className="product-basket" />
+        </div>
+      </div>
+
+      <div className="card">
+        <img
+          src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg"
+          alt=""
+          className="product-img"
+        />
+        <span className="product-title">Nike Air Monarch IV</span>
+        <div className="product-reviews-section">
+          <FaStar className="product-star" />
+          <FaStar className="product-star" />
+          <FaStar className="product-star" />
+          <FaStar className="product-star" />
+          <span>(123 reviews)</span>
+        </div>
+        <div className="product-price">
+          <span>
+            <del>$140.00</del> 200
+          </span>
+          <FaBasketShopping className="product-basket" />
+        </div>
+      </div>
+
+      <div className="card">
+        <img
+          src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg"
+          alt=""
+          className="product-img"
+        />
+        <span className="product-title">Nike Air Monarch IV</span>
+        <div className="product-reviews-section">
+          <FaStar className="product-star" />
+          <FaStar className="product-star" />
+          <FaStar className="product-star" />
+          <FaStar className="product-star" />
+          <span>(123 reviews)</span>
+        </div>
+        <div className="product-price">
+          <span>
+            <del>$140.00</del> 200
+          </span>
+          <FaBasketShopping className="product-basket" />
+        </div>
+      </div>
+
+      <div className="card">
+        <img
+          src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg"
+          alt=""
+          className="product-img"
+        />
+        <span className="product-title">Nike Air Monarch IV</span>
+        <div className="product-reviews-section">
+          <FaStar className="product-star" />
+          <FaStar className="product-star" />
+          <FaStar className="product-star" />
+          <FaStar className="product-star" />
+          <span>(123 reviews)</span>
+        </div>
+        <div className="product-price">
+          <span>
+            <del>$140.00</del> 200
+          </span>
+          <FaBasketShopping className="product-basket" />
+        </div>
+      </div>
+
+      <div className="card">
+        <img
+          src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg"
+          alt=""
+          className="product-img"
+        />
+        <span className="product-title">Nike Air Monarch IV</span>
+        <div className="product-reviews-section">
+          <FaStar className="product-star" />
+          <FaStar className="product-star" />
+          <FaStar className="product-star" />
+          <FaStar className="product-star" />
+          <span>(123 reviews)</span>
+        </div>
+        <div className="product-price">
+          <span>
+            <del>$140.00</del> 200
+          </span>
+          <FaBasketShopping className="product-basket" />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Products;
