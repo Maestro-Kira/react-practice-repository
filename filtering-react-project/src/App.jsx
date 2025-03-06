1a6645b7f43bc163d4aa37d1019ca9bfaef9659f
Maestro-Kira <121399822+Maestro-Kira@users.noreply.github.com>
2025-03-06 22:47:10 +0200

    Finish sidebar section styling

--- filtering-react-project/src/App.jsx
+++ filtering-react-project/src/App.jsx
@@ -2,9 +2,11 @@
 import Navigation from "./Navigation/Navigation";
 import Recommended from "./Recommended/Recommended";
 import Products from "./Products/Products";
+import Sidebar from "./Sidebar/Sidebar";
 function App() {
   return (
     <div>
+      <Sidebar />
       <Navigation />
       <Recommended />
       <Products />
--- filtering-react-project/src/Sidebar/Sidebar.jsx
+++ filtering-react-project/src/Sidebar/Sidebar.jsx
@@ -1,7 +1,81 @@
 import React from "react";
+import "./Sidebar.css";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <>
+      <section className="sidebar-section">
+        <span className="sidebar-cart">🛒</span>
+
+        <div className="sidebar-category-select">
+          <h2 className="sidebar-title">Category</h2>
+          <label className="sidebar-radio-label">
+            <input
+              type="radio"
+              name="category"
+              value=""
+              className="sidebar-radio-input"
+            />
+            <span className="checkmark"></span>
+            All
+          </label>
+          <label>
+            <input type="radio" name="category" value="sneakers" /> Sneakers
+          </label>
+          <label>
+            <input type="radio" name="category" value="flats" /> Flats
+          </label>
+          <label>
+            <input type="radio" name="category" value="sandals" /> Sandals
+          </label>
+          <label>
+            <input type="radio" name="category" value="heels" /> Heels
+          </label>
+        </div>
+
+        <div className="sidebar-category-select">
+          <h2 className="sidebar-title">Price</h2>
+          <label>
+            <input type="radio" name="price" value="" /> All
+          </label>
+          <label>
+            <input type="radio" name="price" value="" /> $0-50
+          </label>
+          <label>
+            <input type="radio" name="price" value="" /> $50-100
+          </label>
+          <label>
+            <input type="radio" name="price" value="" /> $100-150
+          </label>
+          <label>
+            <input type="radio" name="price" value="" /> Over $150
+          </label>
+        </div>
+
+        <div className="sidebar-category-select">
+          <h2 className="sidebar-title">Color</h2>
+          <label>
+            <input type="radio" name="color" value="" /> All
+          </label>
+          <label>
+            <input type="radio" name="color" value="black" /> Black
+          </label>
+          <label>
+            <input type="radio" name="color" value="blue" /> Blue
+          </label>
+          <label>
+            <input type="radio" name="color" value="red" /> Red
+          </label>
+          <label>
+            <input type="radio" name="color" value="green" /> Green
+          </label>
+          <label>
+            <input type="radio" name="color" value="white" /> White
+          </label>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Sidebar;
