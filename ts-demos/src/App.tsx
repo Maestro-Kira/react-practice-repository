8da8ede3d5ad8d4e43bf34d077737d2fd90ce432
Maestro-Kira <121399822+Maestro-Kira@users.noreply.github.com>
2025-03-17 15:57:49 +0200

    Typescript React useState

--- ts-demos/src/App.tsx
+++ ts-demos/src/App.tsx
@@ -1,24 +1,9 @@
-import AdminInfo from "./components/AdminInfo";
-import UserInfo from "./components/UserInfo";
+import Counter from "./components/Counter";
 
 const App = () => {
   return (
     <div>
-      <h1>User info 👇</h1>
-      <UserInfo
-        username="Alex"
-        email="alex@gmail.com"
-        age={20}
-        location={["Earth", "USA"]}
-      />
-      <h1>Admin info 👇</h1>
-      <AdminInfo
-        username="Alex"
-        email="alex@gmail.com"
-        age={20}
-        location={["Mars", "Unkown"]}
-        admin="yes"
-      />
+      <Counter />
     </div>
   );
 };
