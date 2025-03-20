cd4454d6f08e6e1eda0b5a77468a71e2361cfbab
Maestro-Kira <121399822+Maestro-Kira@users.noreply.github.com>
2025-03-20 16:17:59 +0200

    Typescript React UserProfile, Todolist, COunter done

--- ts-demos/src/App.tsx
+++ ts-demos/src/App.tsx
@@ -1,9 +1,11 @@
-import Counter from "./components/Counter";
+import TodoList from "./TodoList";
+import UserProfile from "./UserProfile";
 
 const App = () => {
   return (
     <div>
-      <Counter />
+      <UserProfile />
+      <TodoList />
     </div>
   );
 };
