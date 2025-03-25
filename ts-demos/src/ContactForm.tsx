c1895d1526f12f8e4b37e7a5f3de60d635e9ac2e
Maestro-Kira <121399822+Maestro-Kira@users.noreply.github.com>
2025-03-25 17:58:46 +0200

    Typescript React Useref forms exercise 2 solution variation

--- ts-demos/src/ContactForm.tsx
+++ ts-demos/src/ContactForm.tsx
@@ -1,49 +1,54 @@
-import { FormEvent, useRef, useState } from "react";
-interface UserInfo {
+import { ChangeEvent, FormEvent, useState } from "react";
+
+interface ContactFormState {
   name: string;
   email: string;
-  age: number;
 }
-
 const ContactForm = () => {
-  const [userInfo, setUserInfo] = useState<UserInfo>({
+  const [formData, setFormData] = useState<ContactFormState>({
     name: "",
     email: "",
-    age: 0,
   });
 
-  const name = useRef<HTMLInputElement>(null);
-  const email = useRef<HTMLInputElement>(null);
-  const age = useRef<HTMLInputElement>(null);
-
-  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const nameValue = name.current!.value;
-    const emailValue = email.current!.value;
-    const ageValue = age.current!.value;
-
-    setUserInfo({
-      name: nameValue,
-      email: emailValue,
-      age: +ageValue,
-    });
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
   };
-  return (
-    <div>
-      <br />
-      <form onSubmit={handleSubmit}>
-        <input type="text" placeholder="Please type your name" ref={name} />
-        <input type="email" placeholder="Please type your email" ref={email} />
-        <input type="number" placeholder="Please type your age" ref={age} />
-        <br />
-        <button type="submit">Submit Form</button>
-      </form>
 
-      <h2>User data:</h2>
-      <p>Name : {userInfo.name}</p>
-      <p>Email : {userInfo.email}</p>
-      <p>Age : {userInfo.age}</p>
-    </div>
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("Form Submitted!", formData);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>
+          Name:
+          <input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+          />
+        </label>
+
+        <label>
+          Email:
+          <input
+            type="text"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+        </label>
+      </div>
+
+      <button type="submit">Submit</button>
+    </form>
   );
 };
 export default ContactForm;
