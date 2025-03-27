6d018d35bed717c0c40276097154c5b91a65f012
Maestro-Kira <121399822+Maestro-Kira@users.noreply.github.com>
2025-03-27 17:18:06 +0200

    Typescript React UseReducer practice exercise

--- ts-demos/src/App.tsx
+++ ts-demos/src/App.tsx
@@ -1,15 +1,9 @@
-import { useContext } from "react";
-import Button from "./components/Button";
-import "./index.css";
-import { ThemeContext } from "./context/ThemeContext";
+import Counter from "./components/Counter";
 
 const App = () => {
-  const { theme } = useContext(ThemeContext);
   return (
-    <div
-      className={`main-page ${theme === "light" ? "light-mode" : "dark-mode"}`}
-    >
-      <Button />
+    <div>
+      <Counter />
     </div>
   );
 };
