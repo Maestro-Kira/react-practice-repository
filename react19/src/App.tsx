31e1da6ac9fb9ac4d9f235a009eaecd2c90dcef3
Maestro-Kira <121399822+Maestro-Kira@users.noreply.github.com>
2025-04-04 05:54:37 +0300

    Frame Motion loader variants  example done

--- react19/src/App.tsx
+++ react19/src/App.tsx
@@ -1,11 +1,25 @@
-import BouncingLoader from "./components/BouncingLoader";
-import PulsingButton from "./components/PulsingButton";
+import { motion } from "motion/react";
+import { useState } from "react";
 
 const App = () => {
+  const [isVisible, setIsVisible] = useState(true);
   return (
     <div className="main">
-      {/* <PulsingButton /> */}
-      <BouncingLoader />
+      <motion.div
+        className="circle"
+        variants={{
+          hidden: { opacity: 0, scale: 0.8 },
+          visible: { opacity: 1, scale: 1 },
+          exit: { opacity: 0, scale: 0.5 },
+        }}
+        initial="hidden"
+        animate={isVisible ? "visible" : "hidden"}
+        exit="exit"
+        transition={{
+          duration: 1,
+        }}
+        onClick={() => setIsVisible(!isVisible)}
+      />
     </div>
   );
 };
