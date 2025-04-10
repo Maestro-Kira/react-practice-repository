95fd5241af1aa6d520db6d0b39cc5af7775ff454
Maestro-Kira <121399822+Maestro-Kira@users.noreply.github.com>
2025-04-10 10:28:58 +0300

    Frame Motion stagger gallery done

--- react19/src/App.tsx
+++ react19/src/App.tsx
@@ -1,36 +1,10 @@
-import { motion } from "motion/react";
+import AnimateGallery from "./components/AnimateGallery";
 
 const App = () => {
-  const parentVariants = {
-    hidden: { opacity: 0 },
-    visible: {
-      opacity: 1,
-      transition: {
-        staggerChildren: 0.8,
-      },
-    },
-  };
-
-  const childrenVariants = {
-    hidden: { opacity: 0, y: 20 },
-    visible: { opacity: 1, y: 0 },
-  };
-
   return (
-    <motion.div
-      className="main"
-      variants={parentVariants}
-      initial="hidden"
-      animate="visible"
-    >
-      {[...Array(5)].map((_, index) => (
-        <motion.div
-          key={index}
-          className="circle"
-          variants={childrenVariants}
-        />
-      ))}
-    </motion.div>
+    <div>
+      <AnimateGallery />
+    </div>
   );
 };
 export default App;
