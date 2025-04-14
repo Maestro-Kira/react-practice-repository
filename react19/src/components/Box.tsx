b3881ed9e82749ab117fd2467958e22a38293626
Maestro-Kira <121399822+Maestro-Kira@users.noreply.github.com>
2025-04-14 16:34:39 +0300

    Frame Motion Box move from left screen edge to right  done

---
+++ react19/src/components/Box.tsx
@@ -0,0 +1,26 @@
+import { motion } from "motion/react";
+
+const Box = () => {
+  return (
+    <div className="flex items-center h-screen">
+      <motion.div
+        animate={{
+          x: [0, "calc(100vw - 128px)", 0],
+          rotate: [0, 360, 0],
+          borderRadius: [0, "100%", 0],
+          backgroundColor: ["#000000", "#00FF00", "#000000"],
+        }}
+        transition={{
+          duration: 7,
+          ease: "easeInOut",
+          repeat: Infinity,
+          repeatType: "reverse",
+        }}
+        className="w-32 h-32 bg-black text-white flex justify-center items-center "
+      >
+        <h2>hello there!</h2>
+      </motion.div>
+    </div>
+  );
+};
+export default Box;
