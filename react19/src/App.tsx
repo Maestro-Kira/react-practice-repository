41ab7109618e44e2a250580c445ef5b7e31b9636
Maestro-Kira <121399822+Maestro-Kira@users.noreply.github.com>
2025-04-14 18:12:34 +0300

    Frame Motion spinning icon exercise  done installed frammer motion instead of motion

--- react19/src/App.tsx
+++ react19/src/App.tsx
@@ -1,12 +1,15 @@
 // import Box from "./components/Box";
 
-import Circle from "./components/Circle";
+import SpinningIcon from "./components/SpinningIcon";
+
+// import Circle from "./components/Circle";
 
 const App = () => {
   return (
     <div>
       {/* <Box /> */}
-      <Circle />
+      {/* <Circle /> */}
+      <SpinningIcon />
     </div>
   );
 };
