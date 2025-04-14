eec45e81b2e0e539348ed5622c75a91102dc8670
Maestro-Kira <121399822+Maestro-Kira@users.noreply.github.com>
2025-04-14 18:55:30 +0300

    Frame Motion skew rectangle exercise done

--- react19/src/components/Rectangle.tsx
+++ react19/src/components/Rectangle.tsx
@@ -1,12 +1,19 @@
 import { motion } from "framer-motion";
+import { useState } from "react";
 
 const Rectangle = () => {
+  const [changeRectangle, setChangeRectangle] = useState(false);
   return (
     <div className="h-screen flex items-center justify-center">
       <motion.div
         className="h-28 w-52 bg-green-800"
-        whileTap={{ skewX: 20, skewY: 5 }}
-        transition={{ duration: 1 }}
+        onClick={() => setChangeRectangle(!changeRectangle)}
+        animate={{
+          skewX: changeRectangle ? 20 : 0,
+        }}
+        transition={{
+          duration: 1,
+        }}
       />
     </div>
   );
