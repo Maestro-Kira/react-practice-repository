1116990baa634942d152d3ddddbb7b1917c7bbac
Maestro-Kira <121399822+Maestro-Kira@users.noreply.github.com>
2025-04-14 08:22:13 +0300

    Frame Motion whileInView prop exercise  done

--- react19/src/App.tsx
+++ react19/src/App.tsx
@@ -3,6 +3,8 @@ import AnimateGallery from "./components/AnimateGallery";
 const App = () => {
   return (
     <div>
+      <h1 className="text-center text-3xl mb-4">Scroll me please</h1>
+      <div className="h-screen"></div>
       <AnimateGallery />
     </div>
   );
--- react19/src/components/AnimateGallery.tsx
+++ react19/src/components/AnimateGallery.tsx
@@ -1,39 +1,27 @@
-import { motion, useMotionValue, useTransform } from "framer-motion";
+import { motion } from "motion/react";
 
 const AnimateGallery = () => {
-  const x = useMotionValue(0);
-  const y = useMotionValue(0);
-
-  // Scale changes from 0.5 to 2 based on x position
-  const opacity = useTransform(x, [-100, 100], [0.4, 1]);
-
-  const scale = useTransform(y, [-100, 100], [0.2, 1.2]);
-
   return (
-    <div className="h-screen flex items-center justify-center">
-      <motion.div
-        drag
-        dragConstraints={{ left: -100, right: 100, top: -100, bottom: 100 }}
-        style={{
-          x,
-          y,
-          opacity,
-          scale,
-          width: 100,
-          height: 100,
-          backgroundColor: "skyblue",
-          borderRadius: 20,
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          boxShadow: "0px 4px 10px rgba(0,0,0,0.2)",
-          cursor: "grab",
-        }}
-      >
-        Drag me
-      </motion.div>
+    <div className="flex justify-center items-start mt-[30rem]">
+      <div className="h-[200vh] w-full flex justify-center items-center">
+        <motion.div
+          className="bg-green-600 rounded-lg p-6 shadow-lg text-center"
+          initial={{ scale: 0.5, opacity: 0 }}
+          whileInView={{
+            scale: 1.1,
+            opacity: 1,
+            y: -200,
+            backgroundColor: "orange",
+          }}
+          transition={{ duration: 0.7 }}
+        >
+          <h2 className="text-2xl font-bold mb-2 text-black">Amazing card</h2>
+          <p className="text-black-600">
+            This card animates beautifully intot the view
+          </p>
+        </motion.div>
+      </div>
     </div>
   );
 };
-
 export default AnimateGallery;
