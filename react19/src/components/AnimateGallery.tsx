fb81a5076b31697092e005deb55d9e4926e08741
Maestro-Kira <121399822+Maestro-Kira@users.noreply.github.com>
2025-04-14 14:17:24 +0300

    Frame Motion useScroll useTransform exercise2  done

--- react19/src/components/AnimateGallery.tsx
+++ react19/src/components/AnimateGallery.tsx
@@ -1,17 +1,23 @@
 import { motion, useScroll, useTransform } from "motion/react";
+
 const AnimateGallery = () => {
   const { scrollY } = useScroll();
 
-  const scale = useTransform(scrollY, [0, 300], [1, 1.5]);
-  const opacity = useTransform(scrollY, [0, 300], [1, 0]);
-  return (
-    <div className="h-screen flex items-center justify-center">
-      <motion.div
-        className="bg-blue-500 w-32 h-32 rounded-lg shadow-lg"
-        style={{ scale, opacity }}
-      ></motion.div>
+  const scale = useTransform(scrollY, [0, 1000], [1, 0.5]);
+  const borderRadius = useTransform(scrollY, [0, 1000], ["0%", "50%"]);
 
-      <div className="w-full h-[200vh]"></div>
+  return (
+    <div className="relative h-screen overflow-hidden">
+      <motion.img
+        src="https://images.unsplash.com/photo-1508967497127-90082386bb59?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        alt="Background"
+        className="absolute inset-0 w-full h-full object-cover"
+        style={{ scale, borderRadius }}
+      />
+
+      <div className="sticky top-0 h-screen flex items-center justify-center">
+        <h1 className="text-white text-4xl">Scroll to animate</h1>
+      </div>
     </div>
   );
 };
