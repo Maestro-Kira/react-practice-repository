307b2f7bcef35cd830a1244d8b5e40570c47c4be
Maestro-Kira <121399822+Maestro-Kira@users.noreply.github.com>
2025-04-14 06:02:08 +0300

    Frame Motion useTransform button done

--- react19/src/components/AnimateGallery.tsx
+++ react19/src/components/AnimateGallery.tsx
@@ -1,27 +1,39 @@
-import { motion, useMotionValue, useSpring } from "motion/react";
-import { ChangeEvent } from "react";
+import { motion, useMotionValue, useTransform } from "framer-motion";
 
 const AnimateGallery = () => {
-  const scale = useSpring(1);
+  const x = useMotionValue(0);
+  const y = useMotionValue(0);
 
-  const changeHandler = (event: ChangeEvent<HTMLInputElement>) => {
-    scale.set(parseFloat(event.target.value));
-  };
+  // Scale changes from 0.5 to 2 based on x position
+  const opacity = useTransform(x, [-100, 100], [0.4, 1]);
+
+  const scale = useTransform(y, [-100, 100], [0.2, 1.2]);
+
   return (
-    <div>
-      <motion.div className="circle" style={{ scale }} />
-
-      <div className="mt-[6rem]">
-        <input
-          type="range"
-          min={0.5}
-          max={5}
-          step={0.01}
-          defaultValue={1}
-          onChange={changeHandler}
-        />
-      </div>
+    <div className="h-screen flex items-center justify-center">
+      <motion.div
+        drag
+        dragConstraints={{ left: -100, right: 100, top: -100, bottom: 100 }}
+        style={{
+          x,
+          y,
+          opacity,
+          scale,
+          width: 100,
+          height: 100,
+          backgroundColor: "skyblue",
+          borderRadius: 20,
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          boxShadow: "0px 4px 10px rgba(0,0,0,0.2)",
+          cursor: "grab",
+        }}
+      >
+        Drag me
+      </motion.div>
     </div>
   );
 };
+
 export default AnimateGallery;
