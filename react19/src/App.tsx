2636f3bd82b5267ebc6e4a80f8a908132670b440
Maestro-Kira <121399822+Maestro-Kira@users.noreply.github.com>
2025-04-14 09:14:31 +0300

    Frame Motion useScroll useTransform exercise  done

--- react19/src/App.tsx
+++ react19/src/App.tsx
@@ -3,8 +3,6 @@ import AnimateGallery from "./components/AnimateGallery";
 const App = () => {
   return (
     <div>
-      <h1 className="text-center text-3xl mb-4">Scroll me please</h1>
-      <div className="h-screen"></div>
       <AnimateGallery />
     </div>
   );
--- react19/src/components/AnimateGallery.tsx
+++ react19/src/components/AnimateGallery.tsx
@@ -1,26 +1,17 @@
-import { motion } from "motion/react";
-
+import { motion, useScroll, useTransform } from "motion/react";
 const AnimateGallery = () => {
+  const { scrollY } = useScroll();
+
+  const scale = useTransform(scrollY, [0, 300], [1, 1.5]);
+  const opacity = useTransform(scrollY, [0, 300], [1, 0]);
   return (
-    <div className="flex justify-center items-start mt-[30rem]">
-      <div className="h-[200vh] w-full flex justify-center items-center">
-        <motion.div
-          className="bg-green-600 rounded-lg p-6 shadow-lg text-center"
-          initial={{ scale: 0.5, opacity: 0 }}
-          whileInView={{
-            scale: 1.1,
-            opacity: 1,
-            y: -200,
-            backgroundColor: "orange",
-          }}
-          transition={{ duration: 0.7 }}
-        >
-          <h2 className="text-2xl font-bold mb-2 text-black">Amazing card</h2>
-          <p className="text-black-600">
-            This card animates beautifully intot the view
-          </p>
-        </motion.div>
-      </div>
+    <div className="h-screen flex items-center justify-center">
+      <motion.div
+        className="bg-blue-500 w-32 h-32 rounded-lg shadow-lg"
+        style={{ scale, opacity }}
+      ></motion.div>
+
+      <div className="w-full h-[200vh]"></div>
     </div>
   );
 };
