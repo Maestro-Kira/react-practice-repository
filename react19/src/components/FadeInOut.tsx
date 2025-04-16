e3075e0a7eb521c2b2f754314d5ed2a1c62c9972
Maestro-Kira <121399822+Maestro-Kira@users.noreply.github.com>
2025-04-16 03:52:11 +0300

    Frame Motion fade in fade out exercise 2 done

--- react19/src/components/FadeInOut.tsx
+++ react19/src/components/FadeInOut.tsx
@@ -1,50 +1,41 @@
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import { useState } from "react";
 
 const FadeInOut = () => {
   const [fadeOut, setFadeOut] = useState(false);
 
   return (
-    <div className="flex justify-center items-center h-screen flex-col">
-      <motion.div
-        initial={{
-          y: 20,
-          opacity: 0,
-        }}
-        animate={
-          fadeOut
-            ? { y: 20, opacity: 0 }
-            : {
-                y: 0,
-                opacity: 1,
-              }
-        }
-        transition={{
-          duration: 0.5,
-          ease: "easeInOut",
-        }}
-        whileHover={{
-          scale: 1.07,
-          rotate: -1,
-        }}
-        className={`h-auto w-48 bg-yellow-300 border border-gray-300 shadow-lg p-4 rounded-lg ${
-          fadeOut ? "" : "cursor-pointer"
-        } ${!fadeOut ? "" : "select-none"} mb-4`}
-      >
-        <h2 className="text-center font-bold mb-1">Card Title</h2>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem
-          laboriosam voluptas porro id accusamus non molestias dolorum corrupti
-        </p>
-      </motion.div>
+    <div className="flex flex-col justify-center items-center h-screen">
       <button
-        className="border-2 p-2 m-2"
+        className="px-4 py-2 bg-blue-400 text-white rounded-lg shadow hover:bg-blue-700 transition-colors duration-300 cursor-pointer mb-4"
         onClick={() => {
           setFadeOut(!fadeOut);
         }}
       >
-        Toogle Card
+        Toggle Card
       </button>
+
+      <AnimatePresence>
+        {fadeOut && (
+          <motion.div
+            initial={{
+              opacity: 0,
+            }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.5 }}
+            className="bg-white rounded-xl shadow-md p-6 w-full max-w-70 mb-4 border border-gray-200 "
+          >
+            <h2 className="text-xl font-semibold mb-2 text-gray-800 text-center">
+              Card Title
+            </h2>
+            <p className="text-gray-600">
+              This is some supporting text that goes underneath the title.
+              Clean, readable, and responsive.
+            </p>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 };
