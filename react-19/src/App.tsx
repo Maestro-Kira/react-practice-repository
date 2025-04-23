6e09f99b187415dce290e20339cbe6253bb8efeb
Maestro-Kira <121399822+Maestro-Kira@users.noreply.github.com>
2025-04-23 19:43:55 +0300

    Frame Motion  rotateOnDrag exercise  complete

--- react-19/src/App.tsx
+++ react-19/src/App.tsx
@@ -1,12 +1,15 @@
 // import SwipableCard from "./components/SwipableCard";
 
-import DraggableBox from "./components/DraggableBox";
+import RotateOnDrag from "./components/RotateOnDrag";
+
+// import DraggableBox from "./components/DraggableBox";
 
 const App = () => {
   return (
     <div className="h-screen flex justify-center items-center bg-[#0d1017] text-white">
       {/* <SwipableCard /> */}
-      <DraggableBox />
+      {/* <DraggableBox /> */}
+      <RotateOnDrag />
     </div>
   );
 };
