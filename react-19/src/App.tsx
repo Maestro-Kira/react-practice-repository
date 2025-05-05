d729a695d13b1384eb8febfaa3530f9360d54d24
Maestro-Kira <121399822+Maestro-Kira@users.noreply.github.com>
2025-05-06 01:45:30 +0300

    Complete fetch data exercise

--- react-19/src/App.tsx
+++ react-19/src/App.tsx
@@ -1,9 +1,45 @@
-import TodoList from "./components/TodoList";
+import { useEffect, useState } from "react";
+
+type Product = {
+  id: number;
+  title: string;
+  description: string;
+};
 
 const App = () => {
+  const [serverData, setServerData] = useState<Product[]>([]);
+  const [loadingData, setLoadingData] = useState(false);
+
+  useEffect(() => {
+    async function FetchData() {
+      try {
+        setLoadingData(true);
+        const response = await fetch("https://dummyjson.com/products");
+        const data = await response.json();
+        const { products } = data;
+        setServerData(products);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoadingData(false);
+      }
+    }
+
+    FetchData();
+  }, []);
+
   return (
     <div>
-      <TodoList />
+      {loadingData ? (
+        <p>Loading...</p>
+      ) : (
+        serverData.map((product) => (
+          <div className="mb-4" key={product.id}>
+            <h2 className="text-2xl font-black">{product.title}</h2>
+            <p className="font-light">{product.description}</p>
+          </div>
+        ))
+      )}
     </div>
   );
 };
