1fe1992304dfa7423740a3d748bb195778a93dd3
Maestro-Kira <121399822+Maestro-Kira@users.noreply.github.com>
2025-05-09 19:27:25 +0300

    Complete  motion practive

--- react-19/src/App.tsx
+++ react-19/src/App.tsx
@@ -1,83 +1,64 @@
-import { useEffect, useReducer, useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import { useState } from "react";
+import titleVariants, {
+  buttonContainerVariants,
+  buttonVariants,
+} from "./varients/arients";
 
 const App = () => {
-  const [darkMode, setDarkMode] = useState(() => {
-    return localStorage.getItem("theme") === "dark";
-  });
-
-  useEffect(() => {
-    if (darkMode) {
-      document.documentElement.classList.add("dark");
-      localStorage.setItem("theme", "dark");
-    } else {
-      document.documentElement.classList.remove("dark");
-      localStorage.setItem("theme", "light");
-    }
-  }, [darkMode]);
-
-  type State = {
-    visible: boolean;
-    edited: boolean;
-    title: string;
-  };
-
-  type Action =
-    | { type: "SHOW_ITEM" }
-    | { type: "HIDE_ITEM" }
-    | { type: "EDIT_ITEM" };
-
-  const initialState: State = {
-    visible: true,
-    edited: true,
-    title: "Initial Title",
-  };
-
-  const reducer = (state: State, action: Action) => {
-    switch (action.type) {
-      case "SHOW_ITEM":
-        return { ...state, visible: true };
-      case "HIDE_ITEM":
-        return { ...state, visible: false };
-      case "EDIT_ITEM":
-        return {
-          ...state,
-          edited: !state.edited,
-          title: state.edited ? "Edited Title" : "Initial Title",
-        };
-
-      default:
-        return state;
-    }
-  };
-
-  const [state, dispatch] = useReducer(reducer, initialState);
-
-  console.log(state);
+  const [isVisible, setIsVisible] = useState(true);
 
   return (
-    <div className="main_container space-y-4">
-      <h1 className={`title ${!state.visible ? "invisible" : "visible"}`}>
-        {state.title}
-      </h1>
-      <div className="flex flex-col space-y-2">
-        <button
-          onClick={() => dispatch({ type: "SHOW_ITEM" })}
-          className="btn "
-        >
-          Show Item
-        </button>
-        <button onClick={() => dispatch({ type: "HIDE_ITEM" })} className="btn">
-          Hide Item
-        </button>
-        <button onClick={() => dispatch({ type: "EDIT_ITEM" })} className="btn">
-          Edit Item
-        </button>
-        <button className="btn" onClick={() => setDarkMode(!darkMode)}>
-          {darkMode ? "Toggle Light mode" : "Toggle Dark mode"}
-        </button>
+    <div className="main__container">
+      <div className="title__container">
+        <AnimatePresence>
+          {isVisible && (
+            <motion.h1
+              variants={titleVariants}
+              initial="initial"
+              animate="animate"
+              exit="exit"
+              className="title absolute"
+            >
+              Target text
+            </motion.h1>
+          )}
+        </AnimatePresence>
       </div>
+      <motion.div
+        variants={buttonContainerVariants}
+        initial="initial"
+        animate="animate"
+        className="flex flex-col space-y-2 btn-div"
+      >
+        <motion.button
+          variants={buttonVariants}
+          onClick={() => setIsVisible(!isVisible)}
+          whileHover={{ scale: 1.03 }}
+          whileTap={{ scale: 0.98 }}
+          className="btn"
+        >
+          {isVisible ? "Hide Item" : "Show Item"}
+        </motion.button>
+        <motion.button
+          variants={buttonVariants}
+          whileHover={{ scale: 1.03 }}
+          whileTap={{ scale: 0.98 }}
+          className="btn"
+        >
+          Toggle text
+        </motion.button>
+
+        <motion.button
+          variants={buttonVariants}
+          whileHover={{ scale: 1.03 }}
+          whileTap={{ scale: 0.98 }}
+          className="btn"
+        >
+          Change Theme
+        </motion.button>
+      </motion.div>
     </div>
   );
 };
-
 export default App;
