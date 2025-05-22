afedcfb664a629ac0e8a80111534abc34222513b
Maestro-Kira <121399822+Maestro-Kira@users.noreply.github.com>
2025-05-22 22:30:46 +0300

    Complete custom hook  exercise

--- react-19/src/App.tsx
+++ react-19/src/App.tsx
@@ -1,9 +1,29 @@
-import { Link, Route, Routes, useNavigate } from "react-router-dom";
+import { Link, useNavigate, useRoutes } from "react-router-dom";
 import Recipes from "./pages/recipes/Recipes";
 import Comments from "./pages/comments/Comments";
 import RecipeDetails from "./pages/recipe-details/RecipeDetails";
 import NotFoundPage from "./pages/not-found/NotFoundPage";
 import Layout from "./components/layout/Layout";
+
+function CustomRoutes() {
+  const element = useRoutes([
+    {
+      path: "/home",
+      element: <Layout />,
+      children: [
+        { path: "recipes", element: <Recipes /> },
+        { path: "comments", element: <Comments /> },
+        { path: "recipes/:id", element: <RecipeDetails /> },
+        {
+          path: "*",
+          element: <NotFoundPage />,
+        },
+      ],
+    },
+  ]);
+  return element;
+}
+
 const App = () => {
   const navigate = useNavigate();
   return (
@@ -11,23 +31,23 @@ const App = () => {
       <h1 className="text-2xl mb-2">Main page</h1>
       <div>
         <Link to={"/"}>Navigate to main page</Link>
-        <Link to={"/recipes"}>Navigate to recipe page</Link>
-        <Link to={"/comments"}>Navigate to comments page</Link>
+        <Link to={"/home/recipes"}>Navigate to recipe page</Link>
+        <Link to={"/home/comments"}>Navigate to comments page</Link>
       </div>
       <button
         className="border border-black px-4 py-2 bg-black text-white mr-4 cursor-pointer"
-        onClick={() => navigate("/recipes")}
+        onClick={() => navigate("/home/recipes")}
       >
         Navigate to Recipe
       </button>
       <button
         className="border border-black px-4 py-2 bg-black text-white cursor-pointer"
-        onClick={() => navigate("/comments")}
+        onClick={() => navigate("/home/comments")}
       >
         Navigate to Comments
       </button>
 
-      <Routes>
+      {/* <Routes>
         <Route path="/home" element={<Layout />}>
           <Route path="recipes" element={<Recipes />} />
           <Route path="comments" element={<Comments />} />
@@ -35,7 +55,9 @@ const App = () => {
         </Route>
 
         <Route path="*" element={<NotFoundPage />} />
-      </Routes>
+      </Routes> */}
+
+      <CustomRoutes />
     </div>
   );
 };
--- react-19/src/pages/recipes/Recipes.tsx
+++ react-19/src/pages/recipes/Recipes.tsx
@@ -1,7 +1,35 @@
+import useFetch from "@/hooks/useFetch";
+
+interface Recipe {
+  id: number;
+  name: string;
+  image: string;
+}
+
+interface RecipesResponse {
+  recipes: Recipe[];
+}
+
 const Recipes = () => {
+  const { data, loading } = useFetch<RecipesResponse>(
+    "https://dummyjson.com/recipes"
+  );
+
+  if (loading) return <h1>Fetching recipes</h1>;
+
   return (
     <div>
-      <h1>RECUPY</h1>
+      <h1 className="text-2xl">RECUPY</h1>
+      <ul>
+        {data?.recipes && data.recipes.length > 0
+          ? data?.recipes.map((recipeItem) => (
+              <div>
+                <img src={recipeItem.image} alt="" />
+                <label>{recipeItem.name}</label>
+              </div>
+            ))
+          : null}
+      </ul>
     </div>
   );
 };
